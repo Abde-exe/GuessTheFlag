f781bcc3df019b83aefd98b4cb93fb83f581ffdd
adilelmoba <adilelmoba20@gmail.com>
2023-05-25 18:01:10 +0200

    BOTTOM NAV

--- app/src/main/java/com/example/guesstheflag/MainActivity.kt
+++ app/src/main/java/com/example/guesstheflag/MainActivity.kt
@@ -2,13 +2,40 @@ package com.example.guesstheflag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.guesstheflag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // Adil TOUCH
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        replaceFragment(Home())
+
+        _binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.about -> replaceFragment(About())
+                R.id.list -> replaceFragment(List())
+
+                else -> {
+
+                }
+            }
+
+            true
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        var fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FrameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
